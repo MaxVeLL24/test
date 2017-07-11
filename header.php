dded0a060a25485d31ba55176f62748910a790b9
MaxVeLL24 <pawlenko1993@ukr.net>
2017-07-11 17:02:23 +0300

    without mobile version

---
+++ header.php
@@ -0,0 +1,73 @@
+<header>
+    <div class="header-container">
+        <div class="header-black-string">
+            <div class="col-md-12">
+                <div class="black-string-content">
+                    <ul>
+                        <li><a href="#">Начало</a></li>
+                        <li><a href="#">Контакти</a></li>
+                        <li><a href="#">Статьи</a></li>
+                        <li><a href="#">Статьи</a></li>
+                        <li><a href="#">О компании</a></li>
+                        <li><a href="#">Новости</a></li>
+                        <li><a href="#">Доставка и опалата</a></li>
+                        <li><a href="#">Гарантии</a></li>
+                    </ul>
+                    <ul class="flex-right">
+                        <li><a href=""><i class="russian"></i></a></li>
+                        <li><a href=""><i class="uk"></i></a></li>
+                        <li><a href=""><i class="login"></i>Вход</a></li>
+                        <li><a href="" style="color: #efb247;"><i class="reg"></i>Регистрация</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header-red-container">
+            <div class="col-md-12">
+                <div class="header-red-content" style="display:flex; position: relative">
+                    <div class="logo">
+                        <img src="img/logo.png" alt="">
+                    </div>
+                    <div class="input-group" style="padding-left: 44px;">
+                        <span style="text-decoration: underline;color: #ffffff;font-family: Arial sans-serif;font-size: 12px;float: right">Не дозвонились?</span><br>
+                        <span class="mts" style="float: left"><i class="mtc"></i><span
+                                    style="font-size: 12px;">(050) </span> <span
+                                    style="font-size: 18px;">757-75 77</span></span>
+                        <span class="kyivstar" style="float: right"><i class="kyivstar"></i><span
+                                    style="font-size: 12px;">(067) </span> <span
+                                    style="font-size: 18px;">757-75-77</span></span>
+                        <div class="clear"></div>
+                        <input style="width: 324px;height: 31px;box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.56);border-radius: 3px;background-color: #ffffff;margin-top: 5px;"
+                               type="search" class="form-control" placeholder="Поиск по сайту...">
+                    </div>
+                    <div class="Shopping-basket">
+                        <div class="backet">
+                            <img src="img/1429542581-package.png" alt="Корзина">
+                        </div>
+                        <div class="basket-info">
+                            <span class="basket-title">Корзина</span>
+                            <br><br>
+                            <span>Товаров: 5</span>
+                            <br>
+                            <span>На сумму: 5000 грн</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="col-md-12">
+            <div class="navigation">
+                <ul>
+                    <li><a class="first" href="#">ЖЕНСКАЯ ОДЕЖДА</a></li>
+                    <li><a href="#">МУЖСКАЯ ОДЕЖДА</a></li>
+                    <li><a href="#">ДЕТСКАЯ ОДЕЖДА</a></li>
+                    <li><a href="#">ПОДАРОЧНАЯ КАРТА</a></li>
+                    <li><a href="#">БРЕНДЫ</a></li>
+                    <li><a href="#">НОВИНКИ</a></li>
+                    <li><a href="#">Распродажа</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    </div>
+</header>
---
+++ main-slider.php
@@ -0,0 +1,29 @@
+<div class="col-md-12">
+    <div class="carousel" style="margin: 0 auto; max-width: 1000px; position: relative">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <a href=""><img src="img/slider.png" alt=""></a>
+                    <div class="carousel-caption">
+                    </div>
+                </div>
+                <div class="item">
+                    <a href=""><img src="img/slider.png" alt=""></a>
+                    <div class="carousel-caption">
+
+                    </div>
+                </div>
+            </div>
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <br>
+                <br>
+                <br>
+                <span class="left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
